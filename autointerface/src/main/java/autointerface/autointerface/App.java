ee33f04296831b992364a4052ae6ec28f809e934
Tester <yjsh_ii@163.com>
2021-01-22 11:46:03 +0800

    test

---
+++ autointerface/src/main/java/autointerface/autointerface/App.java
@@ -0,0 +1,13 @@
+package autointerface.autointerface;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
