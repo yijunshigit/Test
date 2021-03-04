69080602d228fa86403f5efe02960d3af86128b3
user1 <yjsh_ii@163.com>
2021-03-05 00:14:22 +0800

    adduser

--- springbootTest/src/main/java/demo/example/server/MyPostMethod.java
+++ springbootTest/src/main/java/demo/example/server/MyPostMethod.java
@@ -1,9 +1,24 @@
 package demo.example.server;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import demo.example.moudel.User;
 
+@RestController
 public class MyPostMethod {
 
-	//@RestController
+	@Autowired
+	private SqlSessionTemplate template1;
+	
+	@RequestMapping(value="addUser",method=RequestMethod.POST)
+	public int addUser(@RequestBody User user){
+		return template1.insert("addUser", user);
+		
+	}
 	
 }
