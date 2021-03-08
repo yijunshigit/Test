019f5092455eed421f747b1099e000fb046b8719
user1 <yjsh_ii@163.com>
2021-03-08 23:03:38 +0800

    zsgc

--- springbootTest/src/main/java/demo/example/server/MyPostMethod.java
+++ springbootTest/src/main/java/demo/example/server/MyPostMethod.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import demo.example.moudel.User;
@@ -15,10 +16,20 @@ public class MyPostMethod {
 	@Autowired
 	private SqlSessionTemplate template1;
 	
-	@RequestMapping(value="addUser",method=RequestMethod.POST)
+	@RequestMapping(value="/addUser",method=RequestMethod.POST)
 	public int addUser(@RequestBody User user){
-		return template1.insert("addUser", user);
+		int result =template1.insert("addUser", user);
+		return result;		
+	}
+	
+	@RequestMapping(value="/updateUser", method=RequestMethod.POST)
+	public int upUser(@RequestBody User user) {
+		return template1.update("updateUser", user);
 		
 	}
 	
+	@RequestMapping(value="/delUser",method=RequestMethod.GET)
+	public int delUser(@RequestParam int id){
+		return template1.delete("delUser", id);
+	}
 }
